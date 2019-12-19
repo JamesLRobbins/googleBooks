94c201188f40ad890692167d966a1113eb69650d
James Robbins <jameslrobbins86@gmail.com>
2019-12-18 23:21:53 -0600

    Final Upload

---
+++ googlebooks/client/src/components/Description/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css"
+
+function Description() {
+  return (
+
+  <div className="container description">
+    <h1 className="display-5">(React) Google Books Search</h1>
+    <p className="lead"><h2>Search for and Save Books of Interest</h2></p>
+  </div>
+      
+  );
+}
+
+export default Description;
---
+++ googlebooks/controllers/googleController.js
@@ -0,0 +1,29 @@
+const axios = require("axios");
+const db = require("../models");
+
+module.exports = {
+    findAll: function(req, res) {
+        const { query: params } = req;
+        axios.get("https://www.googleapis.com/books/v1/volumes?q=" + params + "&key=AIzaSyC_7dqrqP2R8H9_A8SRPxI6Yi1VH74GZUU", {
+            
+        })
+        .then(results => results.data.items.filter(
+            result =>
+            result.volumeInfo.title &&
+            result.volumeInfo.authors &&
+            result.volumeInfo.synopsis
+
+            )
+        
+        )
+        .then(apiBooks =>
+            db.Book.find().then(dbBooks =>
+                apiBooks.filter(apiBook =>
+                    dbBooks.every(dbBook => dbBook.googleId.toString() !== apiBook.id)
+                    )
+                )
+            )
+                 .then(books => res.json(books))
+                 .catch(err => res.status(422).json(err));   
+    }
+};
