d1f6b65c2ab0427e56febe264eaa150b6e766def
James Robbins <jameslrobbins86@gmail.com>
2019-12-18 23:45:42 -0600

    Heroku Push

---
+++ googlebooks/client/build/precache-manifest.d6e51311542f3a4555f4503d961ecb7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b1497edd708e70d74ba",
+    "url": "/static/css/main.c9f0f06b.chunk.css"
+  },
+  {
+    "revision": "9b1497edd708e70d74ba",
+    "url": "/static/js/main.57f3f36c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1ea5ab1d652795807aaf",
+    "url": "/static/js/2.7d42853d.chunk.js"
+  },
+  {
+    "revision": "f25445a40e0d96a9dd3abe00a4e4050c",
+    "url": "/index.html"
+  }
+];
